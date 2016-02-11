a0c88cd691147cd6b76d92b977380cd92760522d
Keegan Bowen <keegan.bowen@gmail.com>
2016-02-10 19:21:49 -0700

    Create init.pp

---
+++ init.pp
@@ -0,0 +1,9 @@
+class timestamped-history {
+  file { '/etc/profile.d/timestamped-history.sh',
+      ensure => file,
+      content=> 'HISTTIMEFORMAT='%m-%d-%Y-%H:%M:%S--',
+      owner  => root,
+      group  => root,
+      mode   => '0755',
+    }
+}
